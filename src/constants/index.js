3bec1fca128b6f4f89c030e44dcc667329f6d6b6
fajarmhmd <111071958+fajarmhmd@users.noreply.github.com>
2023-06-05 13:58:31 +0700

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -44,21 +44,21 @@ export const feedback = [
   {
     id: "feedback-1",
     content: "Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.",
-    name: "Herman Jensen",
-    title: "Founder & Leader",
+    name: "Nasya Nayla",
+    title: "Leader",
     img: people01,
   },
   {
     id: "feedback-2",
     content: "Money makes your life easier. If you're lucky to have it, you're lucky.",
-    name: "Steve Mark",
-    title: "Founder & Leader",
+    name: "Fikri Riyadi",
+    title: "Ceo & Founder",
     img: people02,
   },
   {
     id: "feedback-3",
     content: "It is usually people in the money business, finance, and international trade that are really rich.",
-    name: "Kenn Gallagher",
+    name: "Agus Saryanto",
     title: "Founder & Leader",
     img: people03,
   },
@@ -68,7 +68,7 @@ export const stats = [
   {
     id: "stats-1",
     title: "User Active",
-    value: "9119+",
+    value: "5,000,5+",
   },
   {
     id: "stats-2",
@@ -162,12 +162,12 @@ export const socialMedia = [
   {
     id: "social-media-3",
     icon: twitter,
-    link: "https://www.twitter.com/",
+    link: "https://www.twitter.com/indoviral",
   },
   {
     id: "social-media-4",
     icon: linkedin,
-    link: "https://www.linkedin.com/",
+    link: "https://www.linkedin.com/infolokerkk",
   },
 ];
 
